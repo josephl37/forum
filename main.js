0dcb17f913e44028bdd85dfa9c9d28b035649dee
josephl37 <josephliow2000@gmail.com>
2023-02-01 11:50:22 -0600

    get message and name

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 // your code here
 let button = document.getElementById('submit');
 
+button.addEventListener('click', function () {
+
+  // get message
+  let message = document.getElementById('message').value;
+
+  // get name
+  let name = document.getElementById('name').value;
+
+})
