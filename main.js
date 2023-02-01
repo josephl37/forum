341edd496c13e8ac18cce4e7ae3e15bb3238402b
josephl37 <josephliow2000@gmail.com>
2023-02-01 15:06:49 -0600

    finished and cleaned up project

--- main.js
+++ main.js
@@ -3,24 +3,30 @@ let button = document.getElementById('submit');
 
 button.addEventListener('click', function () {
 
-  // get message
-  let message = document.getElementById('message').value;
+   // create a message post and name post
+   let postMessage = document.createElement('p');
+   let postName = document.createElement('p');
 
-   // turns message into text node
-   let userMessage = document.createTextNode(message)
-
-   // create an individual post
-   let post = document.createElement('p');
+   // create a divider
+   let divider = document.createElement('hr');
 
    // get posts section
    let posts = document.getElementsByClassName("posts")[0];
 
-   // appends message into "posts" div
-   post.appendChild(userMessage);
-   posts.append(post);
+   // get message and name
+   let message = document.getElementById('message').value;
+   let name = document.getElementById('name').value;
 
+   // turns message and name into text node
+   let userMessage = document.createTextNode(message)
+   let userInputName = document.createTextNode(`Posted by: ${name}`);
 
-  // get name
-  let name = document.getElementById('name').value;
+   // appends message and name into "p" element
+   postMessage.appendChild(userMessage);
+   postName.appendChild(userInputName);
 
+  // apends message, name, and divider
+  posts.append(postMessage);
+  posts.append(postName);
+  posts.append(divider);
 })
