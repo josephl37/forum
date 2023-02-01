623e1f105a3a5ff99e89bd8c37f9a23967829d94
josephl37 <josephliow2000@gmail.com>
2023-02-01 12:20:07 -0600

    turns message into text node and append it to posts

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ button.addEventListener('click', function () {
   // get message
   let message = document.getElementById('message').value;
 
+   // turns message into text node
+   let userMessage = document.createTextNode(message)
+
+   // create an individual post
+   let post = document.createElement('p');
+
+   // get posts section
+   let posts = document.getElementsByClassName("posts")[0];
+
+   // appends message into "posts" div
+   post.appendChild(userMessage);
+   posts.append(post);
+
+
   // get name
   let name = document.getElementById('name').value;
 
